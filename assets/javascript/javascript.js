1ec79341b4894e20848e41f10adb7b69fff852b4
Chai1Chai <aasergienko25102005@gmail.com>
2024-01-20 16:38:56 +1000

    3 commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+/*var date = confirm("Всплываюзее окно с выбором");
+if (date){
+    alert("Всплывающее окно");
+}
+
+prompt("ВВеди что-нибудь");
+*/
+function onClickButton() {
+    alert('You click on the button');
+}
